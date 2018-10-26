8ff9d0dec01750f16090d26949f276c04245dc93
KiK0S <kostya.amelichev@gmail.com>
2018-10-26 21:28:27 +0300

    kaggle :(

--- algo/training_contest_4/B.cpp
+++ algo/training_contest_4/B.cpp
@@ -22,85 +22,175 @@ typedef long long ll;
 #ifdef DEBUG
 	const int MAXN = 10;
 #else
-	const int MAXN = 16;
+	const int MAXN = 302;
 #endif
 
 int n, m;
 char g[MAXN][MAXN];
-int ans = 0;
-string res;
+pair<int, bitset<MAXN>> dp[MAXN][MAXN];
+pair<int, bitset<MAXN>> ndp[MAXN][MAXN];
+// int pre[MAXN][MAXN][MAXN];
+int sy = -1, sx = -1;
+
 inline void init() {
-
-}
-
-inline void solve() {
-	init();
-	int start = 0;
 	for (int i = n - 1; i >= 0; i--) {
 		for (int j = 0; j < m; j++) {
 			cin >> g[i][j];
 			if (g[i][j] == 'M') {
-				start = j;
+				sy = j;
+				sx = i;
 			}
 		}
 	}
-	for (int mask = 0; mask < (1 << n); mask++) {
-		for (int submask = mask; submask >= 0; submask = (submask - 1) & mask) {
-			int y = start;
-			int cnt = 0;
-			bool ok = 1;
-			string cur = "";
-			int used = 0;
-			vector<int> open;
-			string all = "";
-			for (int i = 0; i < n; i++) {
-				all.push_back(g[i][y]);
-				if (g[i][y] == '*') {
-					break;
-				}
-				if (g[i][y] == '(') {
-					open.push_back(i);
-				}
-				if (g[i][y] == ')' && open.size()) {
-					used |= (1 << open.back());
-					used |= (1 << i);
-					open.pop_back();
-					cnt += 2;
-				}
-				if (submask & (1 << i)) {
-					y--;
-				}
-				else if (mask & (1 << i)) {
-					y++;
-				}
-				if (y < 0 || y >= m) {
-					break;
-				}
-			}
-			if (ok) {
-				for (int i = 0; i < all.size(); i++) {
-					if (all[i] == '*') {
-						break;
-					}
-					if (used & (1 << i)) {
-						cur.push_back(all[i]);
-					}
-				}
-				if (ans < cnt) {
-					ans = cnt;
-					res = cur;
-				}
-				if (ans == cnt && cur < res) {
-					ans = cnt;
-					res = cur;
-				}
-			}
-			if (submask == 0) {
-				break;
-			}
+	for (int i = 0; i < MAXN; i++) {
+		for (int j = 0; j < MAXN; j++) {
+			dp[i][j].first = -1;
+			ndp[i][j].first = -1;
 		}
 	}
-	cout << ans << '\n' << res << '\n';
+}
+
+bool check (int _a, int _b, bitset<MAXN> &a, bitset<MAXN> &b) {
+	// cerr << _a << ' ' << _b << '\n';
+	// cerr << a << ' ' << b << '\n';
+	if (_b == -1) {
+		return 0;
+	}
+	if (_a <= 0) {
+		return 1;
+	}
+	if (_a != _b) {
+		return _a < _b;
+	}
+	// cerr << a << ' ' << b << '\n';
+	int pos = (a ^ b)._Find_first();
+	return a[pos] < b[pos];
+}
+
+void relax(int tx, int ty, int x, int y, int k) {
+	if (ty < 0) {
+		return;
+	}
+	if (ty == m) {
+		return;
+	}
+	if (g[x][y] == '*') {		
+		return;
+	}
+	if (dp[y][k].first == -1) {
+		return;
+	}
+	if (g[x][y] == '.' || g[x][y] == 'M') {
+		if (check(ndp[ty][k].first, dp[y][k].first, ndp[ty][k].second, dp[y][k].second)) {
+			ndp[ty][k] = dp[y][k];
+			// pre[tx][ty][k] = y;
+		}
+	}
+	if (g[x][y] == '(') {
+		dp[y][k].second[dp[y][k].first] = 1;
+		dp[y][k].first++;
+		if (check(ndp[ty][k + 1].first, dp[y][k].first, ndp[ty][k + 1].second, dp[y][k].second)) {
+			ndp[ty][k + 1] = dp[y][k];
+			// pre[tx][ty][k + 1] = y;
+		}
+		dp[y][k].first--;
+		dp[y][k].second[dp[y][k].first] = 0;
+	}
+	if (g[x][y] == ')' && k > 0) {
+		dp[y][k].first++;
+		if (check(ndp[ty][k - 1].first, dp[y][k].first, ndp[ty][k - 1].second, dp[y][k].second)) {
+			ndp[ty][k - 1] = dp[y][k];
+			// pre[tx][ty][k + 1] = y;
+		}
+		dp[y][k].first--;
+	}
+}
+
+inline void solve() {
+	init();
+	dp[sy][0] = make_pair(0, bitset<MAXN>());
+	int _ans = 0;
+	bitset<MAXN> ans;
+	for (int x = 0; x < n; x++) {
+	// 	for (int i = 0; i <= n; i++) {
+	// 	for (int k = 0; k < m; k++) {
+	// 		cerr << dp[k][i].first << ' ';
+	// 	}
+	// 	cerr << '\n';
+	// }
+		for (int k = 0; k + 1 < n; k++) {
+			for (int y = 0; y < m; y++) {
+				if (k == 0) {
+					if (check(_ans, dp[y][k].first, ans, dp[y][k].second)) {
+						ans = dp[y][k].second;
+						_ans = dp[y][k].first;
+					}
+				}
+				relax(x + 1, y, x, y, k);
+				relax(x + 1, y + 1, x, y, k);
+				relax(x + 1, y - 1, x, y, k);
+			}
+		}
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k + 1 < n; k++) {
+				swap(dp[j][k], ndp[j][k]);
+				ndp[j][k] = make_pair(-1, bitset<MAXN>());
+			}
+		}
+	}	
+	// for (int i = 0; i <= n; i++) {
+	// 	for (int k = 0; k < m; k++) {
+	// 		cerr << dp[k][i].first << ' ';
+	// 	}
+	// 	cerr << '\n';
+	// }
+	// int fx = n, fy = 0;
+	for (int i = 0; i < m; i++) {
+		if (check(_ans, dp[i][0].first, ans, dp[i][0].second)) {
+			ans = dp[i][0].second;
+			_ans = dp[i][0].first;
+			// cerr << _ans << ' ' << ans << '\n';
+			// fx = n;
+			// fy = i;
+		}
+	}
+	if (_ans == 0) {
+		cout << "-1\n";
+		return;
+	}
+	cout << _ans << '\n';
+	for (int i = 0; i < _ans; i++) {
+		if (ans[i]) {
+			cout << '(';
+		}
+		else {
+			cout << ')';
+		}
+	}
+	// cout << ans << '\n';
+	// int bal = 0;
+	// string res;
+	// if (dp[fx][fy][0] < 0) {
+	// 	cout << "-1\n";
+	// 	return;
+	// }
+	// cout << dp[fx][fy][0] << '\n';
+	// while (fx != sx || fy != sy) {
+	// 	// cerr << fx << ' ' << fy << ' ' << bal << '\n';
+	// 	fy = pre[fx][fy][bal];
+	// 	fx--;
+	// 	if (g[fx][fy] == '(') {
+	// 		bal--;
+	// 	}
+	// 	if (g[fx][fy] == ')') {
+	// 		bal++;
+	// 	}
+	// 	if (g[fx][fy] == '(' || g[fx][fy] == ')') {
+	// 		res.push_back(g[fx][fy]);
+	// 	}
+	// }
+	// reverse(res.begin(), res.end());
+	// cout << res << '\n';
 }
 
 signed main() {
--- algo/training_contest_4/C.cpp
+++ algo/training_contest_4/C.cpp
@@ -26,17 +26,17 @@ typedef long long ll;
 #endif
 
 int n, m;
-int k;
+int all;
 int val[MAXN][MAXN];
 int timer[MAXN][MAXN];
-map<deque<pair<int, int>>, int> cur[MAXN][MAXN];
-map<deque<pair<int, int>>, int> pre[MAXN][MAXN];
+int cur[MAXN][MAXN][1 << 14];
+int pre[MAXN][MAXN][1 << 14];
 
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 
 inline void init() {
-
+	all--;
 }
 
 inline void solve() {
@@ -52,56 +52,71 @@ inline void solve() {
 			}
 		}
 	}
-	deque<pair<int, int>> start;
-	start.emplace_back(0, 0);
-	pre[0][0][start] = 1e5 + val[0][0];
-	for (int i = 1; i < k; i++) {
-		for (int x = 0; x < n; x++) {
-			for (int y = 0; y < m; y++) {
-				for (auto it : pre[x][y]) {
-					int vl = it.second;
-					for (int t = 0; t < 4; t++) {
-						int nx = x + dx[t];
-						int ny = y + dy[t];
-						if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+	for (int i = 0; i < (1 << 14); i++) {
+		pre[0][0][i] = 1e5 + val[0][0];
+	}
+	for (int mv = 0; mv < all; mv++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				for (int k = 0; k < 4; k++) {
+					int x = i + dx[k];
+					int y = j + dy[k];
+					// cerr << x << ' ' << y << '\n';
+					if (x < 0 || y < 0 || x >= n || y >= m) {
+						continue;
+					}
+					for (int mask = 0; mask < (1 << min(2 * mv, 14)); mask++) {
+						if (pre[i][j][mask] < 1e5) {
 							continue;
 						}
-						int lft = it.first.size();
+						// cerr << mv << ' ' << i << ' ' << j << ' ' << mask << '\n';
+						int cr = mask;
+						int _dx = 0, _dy = 0;
 						bool ok = 1;
-						for (auto kek : it.first) {
-							if (kek == make_pair(nx, ny) && lft <= timer[nx][ny]) {
+						for (int l = 0; l < min(mv, 7); l++) {
+							int c = cr % 4;
+							// cerr << c << '\n';
+							_dx -= dx[c];
+							_dy -= dy[c];
+							int nx = i + _dx;
+							int ny = j + _dy;
+							if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
 								ok = 0;
+								break;
 							}
-							lft--;
+							// cerr << '\t' << nx << ' ' << ny << ' ' << timer[nx][ny] << '\n';
+							if (nx == x && ny == y && timer[nx][ny] > l + 1) { // mb l + 1
+								ok = 0;
+								break;
+							}
+							cr /= 4;
 						}
 						if (ok) {
-							deque<pair<int, int>> q = it.first;
-							while (q.size() && q.size() > timer[q.front().first][q.front().second]) {
-								q.pop_front();
-							}
-							q.emplace_back(nx, ny);
-							cur[nx][ny][q] = max(cur[nx][ny][q], vl + val[nx][ny]);
+							// cerr << mv << ' ' << i << ' ' << j << ' ' << x << ' ' << y << ' ' << mask << ' ' << (((mask << 2) + k) & ((1 << 16) - 1)) << '\n';
+							cur[x][y][((mask << 2) + k) & ((1 << 14) - 1)] = max(cur[x][y][((mask << 2) + k) & ((1 << 14) - 1)], pre[i][j][mask] + val[x][y]);
 						}
 					}
 				}
 			}
 		}
-		for (int x = 0; x < n; x++) {
-			for (int y = 0; y < m; y++) {
-				cur[x][y].swap(pre[x][y]);
-				cur[x][y].clear();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				for (int mask = 0; mask < (1 << 14); mask++) {
+					swap(cur[i][j][mask], pre[i][j][mask]);
+					cur[i][j][mask] = 0;
+				}
 			}
 		}
 	}
 	int ans = 0;
-	for (int x = 0; x < n; x++) {
-		for (int y = 0; y < m; y++) {
-			for (auto it : pre[x][y]) {
-				ans = max(ans, it.second);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			for (int mask = 0; mask < (1 << 14); mask++) {
+				ans = max(ans, pre[i][j][mask]);
 			}
 		}
 	}
-	cout <<  ans - 1e5 << '\n';
+	cout << ans - 1e5 << '\n';
 }
 
 signed main() {
@@ -114,7 +129,7 @@ signed main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	while (cin >> n >> m >> k)
+	while (cin >> n >> m >> all)
 		solve();
 	return 0;
 }
